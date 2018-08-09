4e42695f7867d0513e89dffb0ebcd49e7724fd53
paul <paul@podotree.com>
2018-08-09 11:17:56 +0900

    staticfiles directory 변경

--- choir/settings/base.py
+++ choir/settings/base.py
@@ -152,6 +152,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join('static'), )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join('static'),)
+STATIC_ROOT = os.path.join('staticfiles')
 
